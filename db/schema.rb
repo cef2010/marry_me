a4da031eccec2a7f49211cb8ef0cc46133d89fb3
cef2010 <cef2010@gmail.com>
2015-10-09 14:48:16 -0600

    update contract columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008231825) do
+ActiveRecord::Schema.define(version: 20151009204515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20151008231825) do
     t.float    "total"
     t.integer  "couple_id"
     t.integer  "vendor_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.boolean  "couple_pending", default: true
+    t.boolean  "vendor_pending", default: true
   end
 
   create_table "couples", force: :cascade do |t|
