9219c18549a5366e32b6f893233c84c128c05df7
jenjen1210 <jen.eutsler@gmail.com>
2015-10-12 14:58:47 -0600

    vendor and contract model and controller tests

--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Contract, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'test model attributes' do
+    it { should respond_to :total }
+    it { should respond_to :couple_id }
+    it { should respond_to :vendor_id }
+    it { should respond_to :couple_pending }
+    it { should respond_to :vendor_pending }
+    it { should belong_to :couple }
+    it { should belong_to :vendor }
+  end
+
+  describe 'model methods' do
+
+    let(:vendor) { FactoryGirl.create(:vendor) }
+    let(:couple) { FactoryGirl.create(:couple) }
+    let(:contract) { FactoryGirl.create(:contract, vendor_id: vendor.id, couple_id: couple.id) }
+
+    it 'creates a contract' do
+      couple
+      vendor
+      contract
+      @new_contract = contract.create_contract(couple, vendor, true, true)
+      expect(@new_contract.couple_pending).to eq(true)
+    end
+
+    it 'updates a contract' do
+      contract
+      contract.approve_contract
+      contract.reload
+      expect(contract.couple_pending).to eq(false)
+    end
+  end
 end
