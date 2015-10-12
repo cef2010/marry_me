dfa462a8a6efa1a1635905080c7c1970eb19f3b2
jenjen1210 <jen.eutsler@gmail.com>
2015-10-12 17:57:25 -0600

    model tests complete controller tests in progress

--- spec/controllers/couples_controller_spec.rb
+++ spec/controllers/couples_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe CouplesController, type: :controller do
-  
-  before(:each) do
+
+  let(:couple) { FactoryGirl.create(:couple) }
 
   describe "GET #index" do
     it "returns http success" do
@@ -13,12 +13,27 @@ RSpec.describe CouplesController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, {id: couple.id}
       expect(response).to have_http_status(:success)
     end
-
-    it 'gets couples with active contracts' do
-      get :show
   end
 
+  # need an edit view to test function
+  # describe "GET #edit" do
+  #   it 'finds the couple to edit' do
+  #     couple
+  #     get :edit, {id: couple.id}
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "PUT #update" do
+  #   it 'updates a couple' do
+  #     couple
+  #     update_params = {id: couple.id, couple: {name: 'Updated Name'}}
+  #     put :update, update_params
+  #     couple.reload
+  #     expect(couple.name).to eq('Updated Name')
+  #   end
+  # end
 end
