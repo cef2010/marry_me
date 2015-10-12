9f88c3d64a7590d87a09e81d483916c56c3705c6
tslater3 <trevor.s.slater@gmail.com>
2015-10-12 14:56:46 -0600

    align factories and couples model testing

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
-
 RSpec.describe Vendor, type: :model do
+
+
   describe "tests model attributes" do
     it { should respond_to :name }
     it { should respond_to :description }
@@ -9,4 +10,5 @@ RSpec.describe Vendor, type: :model do
     it { should respond_to :website }
     it { should respond_to :phone }
   end
+
 end
