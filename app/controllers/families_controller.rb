e67aeece887074ff71660bff00f369f0351d5f2f
cef2010 <cef2010@gmail.com>
2015-10-12 15:12:41 -0600

    add family functionality

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,21 +1,26 @@
 class FamiliesController < ApplicationController
 
   def new
+    @couple = Couple.find(params[:couple_id])
   	@family = Family.new
   end
 
   def create
-    @family = Family.new(family_params)
+    @couple = Couple.find(params[:couple_id])
+    @family = @couple.families.new(family_params)
+    binding.pry
     if @family.save
       redirect_to couple_path(current_couple)
     end
   end
 
   def edit
+    @couple = Couple.find(current_couple)
     @family = Family.find(params[:id])
   end
 
   def update
+    @couple = Couple.find(current_couple)
     @family = Family.find(params[:id])
   	if @family.update(family_params)
   		redirect_to couple_path(current_couple)
