068f9baa2aaa4e1ce7a4cb0b5235d3fecb651a85
tslater3 <trevor.s.slater@gmail.com>
2015-10-12 14:56:46 -0600

    align factories and couples model testing

--- spec/factories/vendors.rb
+++ spec/factories/vendors.rb
@@ -1,11 +1,12 @@
 FactoryGirl.define do
   factory :vendor do
-    name "MyString"
-description "MyText"
-type ""
-address "MyString"
-website "MyString"
-phone "MyString"
+    name "Vendor"
+    description "Vendor"
+    type "Vendor"
+    address "Vendor"
+    website "Vendor"
+    phone "Vendor"
+    sequence(:email) { |n| "vendor#{n}@marryme.com" }
+    password "password"
   end
-
 end
