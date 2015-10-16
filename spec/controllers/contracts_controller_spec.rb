c096f7c42255bbb86e603713ee488fa9367af9cc
jenjen1210 <jen.eutsler@gmail.com>
2015-10-16 16:02:00 -0600

    add params to contracts controller

--- spec/controllers/contracts_controller_spec.rb
+++ spec/controllers/contracts_controller_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ContractsController, type: :controller do
+  let(:vendor) { FactoryGirl.create(:vendor) }
+  let(:couple) { FactoryGirl.create(:couple) }
+  let(:contract) { FactoryGirl.create(:contract, vendor_id: vendor.id, couple_id: couple.id) }
 
+  # ask Dave--"current_couple" causing problems
   describe "GET #index" do
     it "returns http success" do
+      couple
       get :index
       expect(response).to have_http_status(:success)
     end
@@ -18,8 +23,29 @@ RSpec.describe ContractsController, type: :controller do
 
   describe "GET #edit" do
     it "returns http success" do
-      get :edit
+      contract
+      get :edit, {id: contract.id}
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PUT #update" do
+    it 'updates a contract' do
+      contract
+      update_params = {id: contract.id, contract: {vendor_pending: true}}
+      put :update, update_params
+      contract.reload
+      expect(contract.vendor_pending).to eq(true)
+    end
+    # add some verify requirements? Otherwise it always updates
+    # never hits the else in the controller
+  end
+
+  describe "DELETE #destroy" do
+    it 'deletes a contract' do
+      contract
+      delete :destroy, {id: contract.id}
+      expect(Contract.count).to eq(0)
     end
   end
 
