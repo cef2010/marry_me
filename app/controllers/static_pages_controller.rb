c1fbeaa45b7ec3826bffe4bf6f8771c42f93bea1
tslater3 <trevor.s.slater@gmail.com>
2015-10-22 16:07:33 -0600

    contact form, contact model

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,21 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def contact
+    @contact = Contact.new
+  end
+
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to root_path
+    end
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :body)
+    end
 end
