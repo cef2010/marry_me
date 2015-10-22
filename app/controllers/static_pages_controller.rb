76a4c41f3dd31105badf890e247ed2104e5a303e
tslater3 <trevor.s.slater@gmail.com>
2015-10-22 17:21:24 -0600

    flash message fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,7 +14,7 @@ class StaticPagesController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:notice] = "Thanks for contacting us. Someone will contact you shortly."
-      redirect_to root_path
+      redirect_to controller: 'static_pages', action: 'home', flag: true
     end
   end
 
