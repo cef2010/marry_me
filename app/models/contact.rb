cc75344ba52af699c33abf14f1f1b2cbc7ff4f5b
cef2010 <cef2010@gmail.com>
2015-10-27 22:46:02 -0600

    add print method for viewing contacts in IRB

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,11 @@
 class Contact < ActiveRecord::Base
-  
+
+  def self.print
+    self.all.each do |contact|
+      puts "------ CONTACT ID:#{contact.id} ------"
+      puts "FROM: #{contact.name}"
+      puts "EMAIL: #{contact.email}"
+      puts "BODY: #{contact.body}"
+    end
+  end
 end
